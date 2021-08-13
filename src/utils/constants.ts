c27d3dc194c1f85fc074060590d7e664ffa989a1
jjperezaguinaga <me@jjperezaguinaga.com>
2021-08-13 02:37:38 +0000

    chore: This is why we cant have nice things

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,12 +1,12 @@
 import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 
 export namespace constants {
-	export let   BIGINT_ZERO			= BigInt.fromI32(0)
-	export let   BIGINT_ONE				= BigInt.fromI32(1)
-	export let   BIGDECIMAL_ZERO		= new BigDecimal(constants.BIGINT_ZERO)
-	export const ADDRESS_ZERO			= '0x0000000000000000000000000000000000000000'
-	export const BYTES32_ZERO			= '0x0000000000000000000000000000000000000000000000000000000000000000'
-	export const NEW_POST_TYPE			= 'microblog'
-	export const INVALID_POST_TYPE		= 'invalid'
-	export const UNSUPPORTED_POST_TYPE	= 'unsupported'
+	export let BIGINT_ZERO = BigInt.fromI32(0)
+	export let BIGINT_ONE = BigInt.fromI32(1)
+	export let BIGDECIMAL_ZERO = new BigDecimal(constants.BIGINT_ZERO)
+	export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
+	export const BYTES32_ZERO = '0x0000000000000000000000000000000000000000000000000000000000000000'
+	export const NEW_POST_TYPE = 'microblog'
+	export const INVALID_POST_TYPE = 'invalid'
+	export const UNSUPPORTED_POST_TYPE = 'unsupported'
 }
