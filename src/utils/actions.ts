03a31bc0b871c3c17813e5f466494704a857dadc
jjperezaguinaga <me@jjperezaguinaga.com>
2021-10-09 05:31:09 +0000

    hotfix: Lmao assembly script has no try/catch

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -1,5 +1,5 @@
 
-import { json } from "@graphprotocol/graph-ts";
+import { json, log } from "@graphprotocol/graph-ts";
 import { Action, Transaction } from "../../generated/schema";
 import { constants } from "./constants";
 import { parser } from "./parser";
@@ -13,39 +13,45 @@ export namespace actions {
         if (result.isError) {
             return createGenericAction(contentId, constants.INVALID_POST_TYPE)
         }
+        let object = result.value.toObject()
 
-        try {
-            // Valid JSON objects will pass this, but trigger an error when
-            let object = result.value.toObject()
-
-            let action = object.get("type")
-            let actionType = action.toString()
-
-            let text = object.get("text")
-            let textContent = text.toString()
-
-            if (actionType == constants.MICROBLOG_POST_TYPE) {
-                let replyTo = object.get("replyTo")
-                let image = object.get("image")
-                if (replyTo != null) {
-                    let replyToTransactionId = replyTo.toString()
-                    let tx = Transaction.load(replyToTransactionId)
-                    if (tx != null) {
-                        return createReplyToAction(contentId, textContent, replyToTransactionId, constants.MICROBLOG_POST_TYPE)
-                    }
-                }
-                // @TODO For now replies are not able to have images.
-                if (image != null) {
-                    let ipfsHash = image.toString()
-                    if (ipfsHash != null) {
-                        return createNewPostWithImage(contentId, textContent, ipfsHash, constants.MICROBLOG_POST_TYPE)
-                    }
-                }
-                return createTextAction(contentId, textContent, constants.MICROBLOG_POST_TYPE)
-            }
-        } catch {
+        // L#20-27 Valid JSON objects will pass this, but trigger an error when
+        // they are missing a JSON parameter that is then tried to be casted.
+        let action = object.get("type")
+        if (action == null) {
+            log.error('Post with content ID {} has no "type" parameter', [contentId])
             return createGenericAction(contentId, constants.UNSUPPORTED_POST_TYPE)
         }
+        let actionType = action.toString()
+
+        let text = object.get("text")
+        if (text == null) {
+            log.error('Post with content ID {} has no "text" parameter', [contentId])
+            return createGenericAction(contentId, constants.UNSUPPORTED_POST_TYPE)
+        }
+        let textContent = text.toString()
+
+        if (actionType == constants.MICROBLOG_POST_TYPE) {
+            let replyTo = object.get("replyTo")
+            let image = object.get("image")
+            if (replyTo != null) {
+                let replyToTransactionId = replyTo.toString()
+                let tx = Transaction.load(replyToTransactionId)
+                if (tx != null) {
+                    return createReplyToAction(contentId, textContent, replyToTransactionId, constants.MICROBLOG_POST_TYPE)
+                }
+            }
+            // @TODO For now replies are not able to have images.
+            if (image != null) {
+                let ipfsHash = image.toString()
+                if (ipfsHash != null) {
+                    return createNewPostWithImage(contentId, textContent, ipfsHash, constants.MICROBLOG_POST_TYPE)
+                }
+            }
+            return createTextAction(contentId, textContent, constants.MICROBLOG_POST_TYPE)
+        }
+
+
         return createGenericAction(contentId, constants.UNSUPPORTED_POST_TYPE)
     }
 
